ffc49fea0524afe894300f211c4bfdcf80d7b5f2
Valeria Evseenkova <evseenkovavaleria2001@mail.ru>
2022-07-15 17:52:15 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let result = ''
+  let str = n.toString()
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i]
+  }
+  return parseInt(result)
 }
+
